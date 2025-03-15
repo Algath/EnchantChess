b701763ceaad15c9dd16475a2ed1d5668fee5004
NatXiao <natasha.xiao@outlook.com>
2025-03-15 22:57:09 +0100

    small update

--- lan_menu.gd
+++ lan_menu.gd
@@ -67,3 +67,8 @@ func _on_player_connected(id):
 	
 func _on_connection_failed() -> void:
 	print("Connection failed !")
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
+	pass # Replace with function body.
