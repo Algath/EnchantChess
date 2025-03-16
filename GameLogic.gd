817f593fd472cfec104617cd7a9c121bdd0eecd2
NatXiao <natasha.xiao@outlook.com>
2025-03-16 01:09:59 +0100

    function deplacement

--- GameLogic.gd
+++ GameLogic.gd
@@ -1,6 +1,16 @@
 extends Node
 
 var grid1D = []
+var direction := {
+	'NO' = [-1, -1],
+	'N' = [0, -1],
+	'NE' =	[1, -1],
+	'O' = [-1, 0],
+	'E' = [1, 0],
+	'SO' = [-1, 1],
+	'S' = [0, 1],
+	'SE' =[1, 1]
+}
 
 func to2D(grid: Array) -> Array:
 	var res = []
@@ -20,3 +30,7 @@ func to1D(grid: Array) -> Array:
 			res.append(grid[i][j])
 	
 	return res
+
+func TowerDeplacment(position : Array, grid : Array ) -> Array:
+	var res = []
+	return []
